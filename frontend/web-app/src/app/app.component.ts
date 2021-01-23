7bf567904d2b0cd6290006931878dff0a58d92a0
rohanr <rohanrocket4@gmail.com>
2021-01-23 17:00:52 +0530

    feat: Angular material for front end

--- frontend/web-app/src/app/app.component.ts
+++ frontend/web-app/src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-app';
+
+  cities = [
+    {
+      id: 'Bangalore',
+      value: 'Bangalore'
+    },
+    {
+      id: 'Mumbai',
+      value: 'Mumbai'
+    },
+    {
+      id: 'Udupi',
+      value:'Udupi'
+    },
+    {
+      id: 'Kolkata',
+      value: 'Kolkata'
+    },
+    {
+      id: 'Delhi',
+      value: 'Delhi'
+    }
+  ]
+
+ // myControl = new FormControl();
+  options = ['one','two','three']
+
+  displayedColumns = ['Favorites','IFSC','Bank Name','ID','Branch','Address','District','State']
 }
--- frontend/web-app/src/app/app.module.ts
+++ frontend/web-app/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,7 +20,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
